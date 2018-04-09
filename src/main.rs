ea635a252e927eda41a613777e191e3777729343
Jayden Navarro <jdndeveloper@gmail.com>
2018-04-09 16:29:01 -0700

    Added simple server that writes a line then echos input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate tokio;
+
+use tokio::prelude::*;
+use tokio::io;
+use tokio::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1:2345".parse().unwrap();
+    let listener = TcpListener::bind( &address )
+        .expect( "cannot bind TCP listener" );
+
+    // handle incoming connections
+    let server = listener.incoming()
+        .map_err( | e | eprintln!( "accept failed = {:?}", e ) )
+        .for_each( | socket | {
+            // split socket into reader and writer
+            let ( reader, writer ) = socket.split();
+
+            let handle_conn = io::write_all( writer, "hello world\n" )
+                .and_then( | ( writer, _ ) | {
+                    io::copy( reader, writer )
+                } )
+                .then( | _ | {
+                    Ok( () )
+                } );
+
+            // spawn future concurrently
+            tokio::spawn( handle_conn );
+
+            Ok( () )
+        } );
+
+    // start tokio runtime
+    tokio::run( server );
 }
