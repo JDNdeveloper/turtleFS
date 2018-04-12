685f150f17dd3f2d921ec211be31747178b47405
Jayden Navarro <jdndeveloper@gmail.com>
2018-04-12 15:56:22 -0700

    Enhanced stdout logging

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,21 @@ fn main() {
     let listener = TcpListener::bind( &address )
         .expect( "cannot bind TCP listener" );
 
+    println!( "Running file server on {}", address );
+
     // handle incoming connections
     let server = listener.incoming()
         .map_err( | e | eprintln!( "accept failed = {:?}", e ) )
         .for_each( | socket | {
+            let peer_addr = socket.peer_addr().unwrap();
+
             // split socket into reader and writer
             let ( reader, writer ) = socket.split();
 
             let buf: Vec<u8> = Vec::new();
 
             let handle_conn = io::read_to_end( reader, buf )
-                .and_then( | ( _, buf ) | {
+                .and_then( move | ( _, buf ) | {
                     let file_name = std::str::from_utf8( &buf[ .. ] ).unwrap();
 
                     let path = Path::new( file_name );
@@ -31,13 +35,13 @@ fn main() {
                     // open file and read from it
                     let file_oper = match File::open( &file_name ) {
                         Err( why ) => Err( format!(
-                            "couldn't open {}: {}\n", path.display(),
+                            "couldn't open {}: {}", path.display(),
                             why.description() ) ),
                         Ok( mut file ) => {
                             // read file contents into string
                             match file.read_to_string( &mut file_contents ) {
                                 Err( why ) => Err( format!(
-                                    "couldn't read {}: {}\n", path.display(),
+                                    "couldn't read {}: {}", path.display(),
                                     why.description() ) ),
                                 Ok( _ ) => Ok( () ),
                             }
@@ -47,10 +51,13 @@ fn main() {
                     // write file contents (or error) into writer socket
                     match file_oper {
                         Err( why ) => {
-                            eprint!( "{}", why );
-                            io::write_all( writer, why )
+                            eprintln!( "{}: {}", peer_addr, why );
+                            io::write_all( writer, format!( "{}\n", why ) )
                         },
-                        Ok( _ ) => io::write_all( writer, file_contents ),
+                        Ok( _ ) => {
+                            println!( "{}: sending file contents of {}", peer_addr, file_name );
+                            io::write_all( writer, file_contents )
+                        },
                     }
                 } )
                 .then( | _ | {
