f5704a0c72321768a125a133824c34816655e4e5
Jayden Navarro <jdndeveloper@gmail.com>
2018-04-19 14:49:29 -0700

    Added new sample file, and refactored code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate tokio;
 extern crate regex;
 
 use regex::Regex;
-use std::error::Error;
 use std::fs::File;
 use std::path::Path;
 use tokio::prelude::*;
@@ -32,76 +31,6 @@ fn main() {
 
             let handle_conn = io::read_to_end( reader, buf )
                 .and_then( move | ( _, buf ) | {
-                    /* OPERATION FUNCTIONS */
-                    
-                    // write file contents (or error) into writer socket
-                    // and log info
-                    let send_and_log = | oper_result: OperResult | {
-                        match oper_result {
-                            Err( why ) => {
-                                eprintln!( "{}: {}", peer_addr, why );
-                                io::write_all( writer, format!( "{}\n", why ) )
-                            },
-                            Ok( ( result, info ) ) => {
-                                println!( "{}: {}",
-                                           peer_addr, info );
-                                io::write_all( writer, result )
-                            },
-                        }  
-                    };
-
-                    // open file and read from it
-                    let file_read = | file_name, start_offset, end_offset | {
-                        let path = Path::new( file_name );
-                        match File::open( &file_name ) {
-                            Err( why ) => Err( format!(
-                                "couldn't open {}: {}", path.display(),
-                                why.description() ) ),
-                            Ok( mut file ) => {
-                                // read file contents into string
-                                let mut file_contents = String::new();
-                                let file_read_oper =
-                                    match file.read_to_string( &mut file_contents ) {
-                                        Err( why ) => Err( format!(
-                                            "couldn't read {}: {}", path.display(),
-                                            why.description() ) ),
-                                        Ok( _ ) => Ok( () ),
-                                    };
-                                // handle start offset
-                                file_contents = file_contents
-                                    .chars()
-                                    .skip( start_offset as usize )
-                                    .collect();
-                                // handle end offset
-                                if end_offset != -1 {
-                                    file_contents = file_contents
-                                        .chars()
-                                        .take( end_offset as usize -
-                                               start_offset as usize )
-                                        .collect();
-                                }
-                                match file_read_oper {
-                                    Err( why ) => Err( why ),
-                                    Ok( _ ) => Ok( ( file_contents, format!(
-                                        "sending file contents of {}, \
-                                         start offset {}, end offset {}",
-                                        file_name, start_offset, end_offset ) ) ),
-                                }
-                            },
-                        }
-                    };
-                    
-                    let file_length = | file_name | {
-                        match std::fs::metadata( file_name ) {
-                            Err( why ) => Err( format!(
-                                "couldn't get info for {}: {}",
-                                file_name, why.description() ) ),
-                            Ok( m ) => Ok( ( format!( "{}", m.len() ),
-                                             format!( "sending file length of {}: {}",
-                                                       file_name, m.len() ) ) )
-                        }
-                    };
-
                     /* ERROR MESSAGES */
                     
                     let format_err = Err( format!(
@@ -122,6 +51,78 @@ fn main() {
 
                     let unrecognized_action_err = | action | {
                         Err( format!( "unrecognized action: {}", action ) )
+                    };
+
+                    let no_info_err = | file_name, why | {
+                        Err( format!(
+                            "could not retrieve file metadata for {}: {}",
+                            file_name, why ) )
+                    };
+                    
+                    /* OPERATION FUNCTIONS */
+                    
+                    // write file contents (or error) into writer socket
+                    // and log info
+                    let send_and_log = | oper_result: OperResult | {
+                        match oper_result {
+                            Err( why ) => {
+                                eprintln!( "{}: {}", peer_addr, why );
+                                io::write_all( writer, format!( "{}\n", why ) )
+                            },
+                            Ok( ( result, info ) ) => {
+                                println!( "{}: {}",
+                                           peer_addr, info );
+                                io::write_all( writer, result )
+                            },
+                        }  
+                    };
+
+                    // open file and read from it
+                    let file_read_func = | file_name, start_offset, end_offset | {
+                        let path = Path::new( file_name );
+                        match File::open( &file_name ) {
+                            Err( why ) => Err( format!(
+                                "couldn't open {}: {}", path.display(),
+                                why ) ),
+                            Ok( mut file ) => {
+                                // read file contents into string
+                                let mut file_contents = String::new();
+                                let file_read_oper =
+                                    match file.read_to_string( &mut file_contents ) {
+                                        Err( why ) => Err( format!(
+                                            "couldn't read {}: {}", path.display(),
+                                            why ) ),
+                                        Ok( _ ) => Ok( () ),
+                                    };
+                                // handle start offset
+                                file_contents = file_contents
+                                    .chars()
+                                    .skip( start_offset as usize )
+                                    .collect();
+                                // handle end offset
+                                file_contents = file_contents
+                                    .chars()
+                                    .take( end_offset as usize -
+                                           start_offset as usize )
+                                    .collect();
+                                match file_read_oper {
+                                    Err( why ) => Err( why ),
+                                    Ok( _ ) => Ok( ( file_contents, format!(
+                                        "sending file contents of {}, \
+                                         start offset {}, end offset {}",
+                                        file_name, start_offset, end_offset ) ) ),
+                                }
+                            },
+                        }
+                    };
+                    
+                    let file_length_func = | file_name | {
+                        match std::fs::metadata( file_name ) {
+                            Err( why ) => no_info_err( file_name, why ),
+                            Ok( m ) => Ok( ( format!( "{}", m.len() ),
+                                             format!( "sending file length of {}: {}",
+                                                       file_name, m.len() ) ) )
+                        }
                     };
 
                     /* REQUEST PARSING */
@@ -161,43 +162,70 @@ fn main() {
                     let action = actions_vec[ 0 ];
                     let action_args = &actions_vec[ 1 .. ];
 
-                    /* EXECUTE AND RETURN REQUEST */
+                    // we need file length for a couple of different things,
+                    // so we go ahead and retrieve that now
+                    let file_length = match std::fs::metadata( file_name ) {
+                        Ok( m ) => m.len() as u32,
+                        Err( why ) => {
+                            return send_and_log( no_info_err(
+                                file_name, why ) );
+                        },
+                    };
+                    
+                    /* EXECUTE AND RETURN RESPONSE */
 
                     if action == "READ" {
                         let mut start_offset = 0;
-                        let mut end_offset = -1;
+                        let mut end_offset = file_length;
                         if action_args.len() >= 1 {
                             let start_offset_str = action_args[ 0 ];
                             start_offset =
-                                match start_offset_str.parse::< i32 >() {
+                                match start_offset_str.parse::< u32 >() {
                                     Ok( n ) => n,
                                     Err( why ) => {
                                         return send_and_log( num_arg_err(
-                                            start_offset_str, why ) );
+                                            String::from( start_offset_str ),
+                                            why ) );
                                     },
                                 };
+                            if start_offset >= file_length {
+                                return send_and_log( Err( format!(
+                                    "start offset ({}) must be: \
+                                     less than file length ({})",
+                                    start_offset, file_length ) ) );
+                            }
                         }
                         if action_args.len() >= 2 {
                             let end_offset_str = action_args[ 1 ];
                             end_offset =
-                                match end_offset_str.parse::< i32 >() {
+                                match end_offset_str.parse::< u32 >() {
                                     Ok( n ) => n,
                                     Err( why ) => {
                                         return send_and_log( num_arg_err(
-                                            end_offset_str, why ) );
+                                            String::from( end_offset_str ),
+                                            why ) );
                                     },
-                                };                                
+                                };
+                            if end_offset < 1 || end_offset > file_length ||
+                                end_offset <= start_offset {
+                                return send_and_log( Err( format!(
+                                    "end offset ({}) must be: \
+                                     greater than zero, \
+                                     greater than start offset ({}), \
+                                     less than or equal to file length ({})",
+                                    end_offset, start_offset, file_length ) ) );
+                            }
                         }
                         if action_args.len() > 2 {
                             return send_and_log(
                                 too_many_args_err( action_args.len(), action ) );
                         }
-                        return send_and_log( file_read( file_name,
-                                                        start_offset,
-                                                        end_offset ) );
+                        return send_and_log( file_read_func( file_name,
+                                                             start_offset,
+                                                             end_offset ) );
                     } else if action == "LENGTH" {
                         if action_args.len() == 0 {
-                            return send_and_log( file_length( file_name ) );
+                            return send_and_log( file_length_func( file_name ) );
                         } else {
                             return send_and_log(
                                 too_many_args_err( action_args.len(), action ) );
