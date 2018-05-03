10c1f992ec83809961587098698f8fd02dc2ff29
Jayden Navarro <jdndeveloper@gmail.com>
2018-05-03 11:34:02 -0700

    Fixed binary files not printed properly issue

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,18 +1,17 @@
 use std::io::prelude::*;
+use std::io::{self, Write};
 use std::net::TcpStream;
 
 fn main() {
     let mut stream = TcpStream::connect( "127.0.0.1:2345" ).unwrap();
 
     let request: &[u8] = b"/Users/jayden/rust_projects/file_server/\
-                           src/example_files/hello.txt:(READ)";
+                           src/example_files/hello.zip:(READ)";
     let response: &mut Vec<u8> = &mut Vec::new();
 
     let _ = stream.write( request );
     stream.shutdown( std::net::Shutdown::Write ).unwrap();
     let _ = stream.read_to_end( response );
 
-    let s = String::from_utf8_lossy( response );
-
-    print!( "{}", s );
+    io::stdout().write( response ).unwrap();
 }
