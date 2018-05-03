bd32876d945f790c5f58ea0a0bbcd5f84932e0ce
Jayden Navarro <jdndeveloper@gmail.com>
2018-05-03 11:17:08 -0700

    Implemented very simple client

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,18 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
-    println!( "Hello, I am a client!" );
+    let mut stream = TcpStream::connect( "127.0.0.1:2345" ).unwrap();
+
+    let request: &[u8] = b"/Users/jayden/rust_projects/file_server/\
+                           src/example_files/hello.txt:(READ)";
+    let response: &mut Vec<u8> = &mut Vec::new();
+
+    let _ = stream.write( request );
+    stream.shutdown( std::net::Shutdown::Write ).unwrap();
+    let _ = stream.read_to_end( response );
+
+    let s = String::from_utf8_lossy( response );
+
+    print!( "{}", s );
 }
